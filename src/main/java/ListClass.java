17cb463a8bcf7c5b06d57cf42cbc30fdb93ce35d
Liyabona Saki <liyabonasaki@gmail.com>
2021-05-04 16:05:39 +0200

    [Initial] commit

---
+++ src/main/java/ListClass.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListClass {
+    //List
+
+
+    List StudentNames = new ArrayList();
+
+    public void add(){
+        StudentNames.add(0,"Amahle");
+        StudentNames.add(1,"Liyabona");
+        StudentNames.add(2,"Jon");
+        StudentNames.add(3,"Suski");
+
+    }
+}
