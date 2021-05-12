a8171e1c44dc415906c846618f657722b067cfe7
Liyabona Saki <liyabonasaki@gmail.com>
2021-05-12 15:38:24 +0200

    [Update] Final Version

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ADP3_Assignment2
+
+Read Me:
+
+1)To run the application pls uncomment the section you want to test
+
+2)You can use your own values that's not an issue.
--- src/main/java/za/ac/cput/adp3/Main.java
+++ src/main/java/za/ac/cput/adp3/Main.java
@@ -5,52 +5,47 @@ Liyabona Saki
 
 package za.ac.cput.adp3;
 
-import za.ac.cput.adp3.CollectionsClass;
-import za.ac.cput.adp3.ListClass;
-
 public class Main {
 
     public static void main(String[] args) {
 
+        /*
+        Read Me:
+        1)To run the application pls uncomment the section you want to test
+        2) You can use your own values that's not an issue.
+         */
 
-        ListClass lc = new ListClass();
 
+        ListClass lc = new ListClass(); //list class
 //        lc.add("Liyabona");
 //        lc.add("Beauty");
 //        lc.add("kusie");
-
 //        lc.remove("Liyabona");
-
 //        lc.find("Liyabona");
 
 
-        SetClass sc = new SetClass();
+        SetClass sc = new SetClass(); //set class
 //        sc.add("Melo");
 //        sc.add("Liyabona");
 //        sc.add("kusie");
-
 //        sc.remove("Melo");
-
 //        sc.find("Liyabona");
-//
-       MapClass mc = new MapClass();
+
+
+       MapClass mc = new MapClass(); //map class
 //        mc.add(10,"Liyabona");
 //        mc.add(11,"Saki");
 //        mc.add(5,"kusie");
-
 //        mc.remove(5);
-
 //        mc.find("Liyabona");
 
 
 
-        CollectionsClass cc = new CollectionsClass();
+        CollectionsClass cc = new CollectionsClass(); //collection class
 //        cc.add("Liyabona");
 //        cc.add("master");
 //        cc.add("Susie");
-//
 //        cc.remove("Liyabona");
-//
 //        cc.find("test");
 
 
